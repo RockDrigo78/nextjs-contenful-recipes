0841912ee4a993239cfa9ddede85b8dfb2600738
Rodrigo Bazan Danino <rbazandanino@deloitte.de>
2022-09-18 23:14:59 +0200

    add loading fallback page

--- pages/recipes/[slug].jsx
+++ pages/recipes/[slug].jsx
@@ -1,6 +1,7 @@
 import { createClient } from 'contentful';
 import Image from 'next/image';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import Loading from '../../components/Loading';
 
 const client = createClient({
     space: process.env.CONTENTFUL_SPACE_ID,
@@ -22,7 +23,7 @@ export const getStaticPaths = async () => {
 
     return {
         paths,
-        fallback: false,
+        fallback: true,
     };
 };
 
@@ -41,6 +42,7 @@ export const getStaticProps = async ({ params }) => {
 };
 
 export default function RecipeDetails({ recipe }) {
+    if (!recipe) return <Loading />;
     const { title, ingredients, method, coockingTime, featuredImage } =
         recipe.fields;
 
